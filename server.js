16b0a3c372c735e76244200dc9cccd373fc7792f
Joshua Yoes <>
2020-04-01 12:20:20 -0700

    Added validation, blank route, and custom params

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-require('dotenv').config();
 var express = require('express');
 var app = express();
-
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+var moment = require('moment');
+moment().format();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -17,17 +12,42 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  const { date_string } = req.params;
+
+  const userDate = moment(date_string);
+
+  if (!userDate.isValid()) {
+    return res.status(400).json({
+      "error": "Invalid Date" 
+    })
+  }
+
+  if (!date_string) {
+    const utcDate = new Date(Date.now()).toUTCString();
+
+    return res.status(200).json({
+      "unix": Date.now(),
+      "utc": utcDate,
+    })
+  }
+
+  const unixFormat = new Date(date_string).getTime();
+  const utcFormat = new Date(date_string).toUTCString();
+
+  res.json({
+    unix: unixFormat,
+    utc: utcFormat
+  });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Listening on port ' + listener.address().port + '...');
 });
