dac052122437edb66c084a23ecdae9d285086823
Joshua Yoes <>
2020-04-01 17:53:44 -0700

    Refactor for error code response

--- server.js
+++ server.js
@@ -1,57 +1,60 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var moment = require('moment');
+var moment = require("moment");
 moment().format();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// returns current time
+app.get("/api/timestamp", (req, res) => {
+  const utcDate = new Date(Date.now()).toUTCString();
 
-// your first API endpoint... 
-app.get("/api/timestamp/:date_string?", (req, res) => {
+  res.status(200).json({
+    unix: Date.now(),
+    utc: utcDate
+  });
+});
+
+// returns time if passed ISO or Unix via URL params
+app.get("/api/timestamp/:date_string", (req, res) => {
   let { date_string } = req.params;
 
-  //tests to see if input is unix timestamp (seconds)
-  const containsNonInts = /[^\d]/g.test(date_string)
-
-  if (!containsNonInts) {
-    // Passed unix in seconds, converted to milliseconds
-    date_string = parseInt(date_string * 1000);
+  // tests to see if input is unix timestamp or iso timestamp 
+  const isUnixTimestamp = /\d{5,}/g.test(date_string);
+    
+  // transform pure number param (i.e. Unix timestamp) from string to integer
+  // i.e. "/api/timestamp/1450137600000"
+  if (isUnixTimestamp) {
+    let date_int = parseInt(date_string);
+    
+    return res.json({
+      unix: date_int,
+      utc: new Date(date_int).toUTCString()
+    })
   }
-
-  // Uses moment.js library
-  // https://momentjs.com/docs/#/parsing/is-valid/
-  // TODO: find way to avoid deprecation warning in log
-  const validDateInput = moment(date_string).isValid();
-
-  if (!validDateInput) {
-    return res.status(400).json({
-      "unix": "null",
-      "utc": "Invalid Date"
+  
+  // check to see param is valid for Date constructor 
+  const validDateInput = new Date(date_string).toString();
+  
+  // send error response if invalid param
+  if (validDateInput == "Invalid Date") {
+    return res.json({
+      error: "Invalid Date"
     })
   }
 
-  // GET: /api/timestamp
-  // returns current time if not date_string passed in params
-  if (!date_string) {
-    const utcDate = new Date(Date.now()).toUTCString();
-
-    return res.status(200).json({
-      "unix": Date.now(),
-      "utc": utcDate,
-    })
-  }
-
+  // assume at this point, param is ISO format
   const unixFormat = new Date(date_string).getTime();
   const utcFormat = new Date(date_string).toUTCString();
 
@@ -61,7 +64,7 @@ app.get("/api/timestamp/:date_string?", (req, res) => {
   });
 });
 
-// listen for requests 
+// listen for requests
 var listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port ' + listener.address().port + '...');
+  console.log("Listening on port " + listener.address().port + "...");
 });
